94d95fec43a2a3912c1b2ee4da4167377270e031
Josue Abraham <josuea@noukod.com>
2018-10-19 13:22:45 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -1,17 +1,23 @@
 class MusicImporter
-  attr_accessor :path
+
+  attr_reader :path
 
   def initialize(path)
     @path = path
   end
 
+  # Instance Methods
   def files
-    Dir[File.join(@path, '*.mp3')].map do |file|
-      File.basename file
+    file_list = []
+    Dir.foreach(path) do |file|
+      if file.end_with?("mp3")
+        file_list << file
+      end
     end
+    file_list
   end
 
   def import
-    files.each { |file| Song.create_from_filename(file) }
+    files.each {|filename| Song.create_from_filename(filename)}
   end
 end
